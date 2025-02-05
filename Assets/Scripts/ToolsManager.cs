86a11dd362df64b189b9f8641eb8c1c0ae9465fb
mohasimkhan <ak4514768@gmail.com>
2025-02-05 12:51:14 +0530

    Fixed control panel ref issue on changing controls

--- Assets/Scripts/ToolsManager.cs
+++ Assets/Scripts/ToolsManager.cs
@@ -4,14 +4,42 @@ using UnityEngine.UI;
 
 public class ToolsManager : MonoBehaviour
 {
-    public Slider altitude;
-    public Slider heading;
-    public TMP_Text altitudeText;
+
+    public Slider controllerAltitude;
+    public Slider controllerHeading;
+    public TMP_Text controllerAltitudeText;
+    public Slider handAltitude;
+    public Slider handHeading;
+    public TMP_Text handAltitudeText;
     public GameObject headingNeedle;
 
-    private void Start()
+    private Slider altitude;
+    private Slider heading;
+    private TMP_Text altitudeText;
+
+    public void SetHandElements()
     {
+        altitude = handAltitude;
+        heading = handHeading;
+        altitudeText = handAltitudeText;
+        FixListeners();
+    }
+
+    public void SetControllerElements()
+    {
+        altitude = controllerAltitude;
+        heading = controllerHeading;
+        altitudeText = controllerAltitudeText;
+        FixListeners();
+    }
+
+
+    private void FixListeners()
+    {
+        // Remove old listeners from previous controls and add new ones from current controls
+        altitude.onValueChanged.RemoveAllListeners();
         altitude.onValueChanged.AddListener(OnAltitudeChanged);
+        heading.onValueChanged.RemoveAllListeners();
         heading.onValueChanged.AddListener(OnHeadingChanged);
     }
 
